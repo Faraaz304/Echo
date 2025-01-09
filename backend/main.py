05e2b1da09f5d2cac846487c1b55bbc84ac28150
Raahim <144481724+Faraaz304@users.noreply.github.com>
2025-01-09 20:44:47 +0530

    flsk

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return jsonify(message="Hello, World!")
 
 if __name__ == '__main__':
     app.run(debug=True)
